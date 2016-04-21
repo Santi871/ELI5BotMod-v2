175fe3af2c0c6859234d66db47477fe41badc18c
Santi871 <santivegega@gmail.com>
2016-04-21 00:03:14 -0300

    added except TypeError pass

--- modules/commands.py
+++ modules/commands.py
@@ -52,6 +52,8 @@ class Commands:
         except AttributeError:
             self.s.send_msg('Command not recognized. Enter !commands for a list of commands',
                             channel_name=slack_event.get('channel'))
+        except TypeError:
+            pass
 
     #  ----------- DEFINE COMMANDS HERE -----------
 
