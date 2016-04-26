6bd98e7227b9ccc56a39f23fe68052da79ed3f80
Santi871 <santivegega@gmail.com>
2016-04-26 15:00:43 -0300

    changed !addrule to !rules add

--- modules/commands.py
+++ modules/commands.py
@@ -271,9 +271,7 @@ class CommandsHandler:
     def rules(self, *args):
 
         """!rules add [words to be filtered]: Creates a rule to filter new questions that contain
-        ALL the words passed
-
-        """
+        ALL the words passed"""
 
         slack_args = args[1]
         split_text = slack_args['text'].split()
