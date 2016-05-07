4b9c87dcaa9c3bca09b44467b707b5af111fb499
Santi871 <santivegega@gmail.com>
2016-05-06 23:57:34 -0300

    changed search to month

--- modules/filters.py
+++ modules/filters.py
@@ -187,8 +187,7 @@ class Filters:
             while True:
 
                 try:
-                    search_result = self.r.search(full_search_query, subreddit=self.subreddit, sort='new',
-                                                  period='month')
+                    search_result = self.r.search(full_search_query, subreddit=self.subreddit, sort='new')
                     search_result_list = list(search_result)
                     break
                 except AssertionError:
