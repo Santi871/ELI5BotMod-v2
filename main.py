9e242f54ccc96ebb8c1523a6d58a837760e79836
Santi871 <santivegega@gmail.com>
2016-06-23 00:54:14 -0300

    added flask

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import os
 import time
 from slacksocket import SlackSocket
 from configparser import ConfigParser
+from flask import Flask, request, Response
+
+
+app = Flask(__name__)
+
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
+
+
+@app.route('/slack', methods=['POST'])
+def inbound():
+    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
+        channel = request.form.get('channel_name')
+        username = request.form.get('user_name')
+        text = request.form.get('text')
+        inbound_message = username + " in " + channel + " says: " + text
+        print(inbound_message)
+    return Response(), 200
+
+
+@app.route('/', methods=['GET'])
+def test():
+    return Response('It works!')
+
+
+# if __name__ == "__main__":
+#     app.run(threaded=True)
 
 
 def main():
@@ -11,6 +37,7 @@ def main():
     config = ConfigParser()
     config.read('config.ini')
     default_channel = config.get('slack', 'default_channel')
+    app.run(threaded=True)
 
     # Create a SlackSocket instance with select filters
     event_filters = ['message']
