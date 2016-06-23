10616169d7d35c3fcbd628bdaac26db06c991ad0
Santi871 <santivegega@gmail.com>
2016-06-23 02:36:03 -0300

    added flask

--- main.py
+++ main.py
@@ -35,7 +35,10 @@ def test():
 def command():
     print(str(request.form))
     if request.form.get('token') == SLACK_SLASHCMDS_SECRET:
-        response = [{
+        response = {
+                "text": "Would you like to play a game?",
+                "attachments": [
+        {
             "text": "Choose a game to play",
             "fallback": "You are unable to choose a game",
             "callback_id": "wopr_game",
@@ -68,7 +71,9 @@ def command():
                     }
                 }
             ]
-        }]
+        }
+    ]
+}
 
         return Response(response=json.dumps(response), status=200, mimetype='application/json')
 
