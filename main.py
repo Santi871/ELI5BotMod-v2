5ec78c25df34f5549bb828736d300ed838112bb0
Santi871 <santivegega@gmail.com>
2016-06-23 02:28:36 -0300

    added flask

--- main.py
+++ main.py
@@ -36,14 +36,39 @@ def command():
     print(str(request.form))
     if request.form.get('token') == SLACK_SLASHCMDS_SECRET:
         response = {
-                        "response_type": "in_channel",
-                        "text": "It's 80 degrees right now.",
-                        "attachments": [
-                                            {
-                                                "text": "Partly cloudy today and tomorrow"
-                                            }
-                                        ]
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "chess",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "maze",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "war",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
                     }
+                }
+            ]
+        }
 
         return Response(response=json.dumps(response), status=200, mimetype='application/json')
 
